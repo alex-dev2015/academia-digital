c0cf75d1c975dabb4307f01c38d688fd5cbdae4f
AlexSousa <alex.sousa20@hotmail.com>
2022-01-31 16:57:50 -0300

    First commit

---
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -0,0 +1,17 @@
+package me.dio.academia.digital.entity.form;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class AlunoForm {
+
+  private String nome;
+
+  private String cpf;
+
+  private String bairro;
+
+  private LocalDate dataDeNascimento;
+}

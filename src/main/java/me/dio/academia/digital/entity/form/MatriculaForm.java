4458e77380b6beebeda40338da8c60a346eaeca2
AlexSousa <alex.sousa20@hotmail.com>
2022-02-03 17:49:00 -0300

    Implementação da Matrícula

--- src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -1,5 +1,8 @@
 package me.dio.academia.digital.entity.form;
 
+import lombok.Data;
+
+@Data
 public class MatriculaForm {
 
   private Long alunoId;
--- src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
+++ src/main/java/me/dio/academia/digital/repository/MatriculaRepository.java
@@ -1,4 +1,7 @@
 package me.dio.academia.digital.repository;
 
-public interface MatriculaRepository {
+import me.dio.academia.digital.entity.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
 }
